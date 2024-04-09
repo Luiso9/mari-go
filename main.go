fed6a55e5862fcfcbee124007c7c5850ac670f52
driannsa <muhandreop@gmail.com>
2024-04-09 20:34:02 +0700

    Update main.go: load config from json file, use bwmarrin/discordgo package, handle SIGINT and SIGTERM
    The changes include:
    
    - Load configuration from a JSON file (config.json) instead of using the alfredosa/GoDiscordBot/config package.
    - Replace the previous bot package with the bwmarrin/discordgo package for Discord API interaction.
    - Add a handler for SIGINT, SIGTERM, and os.Interrupt signals to gracefully shut down the bot.
    
    The new code reads the token from the config.json file and initializes the discordgo session with it. The bot responds to "hello" messages with "World!". The program now listens for SIGINT, SIGTERM, and os.Interrupt signals and exits gracefully when any of these signals are received.

--- main.go
+++ main.go
@@ -1,22 +1,78 @@
 package main
 
 import (
- "fmt"
- "github.com/alfredosa/GoDiscordBot/config"
- "github.com/alfredosa/GoDiscordBot/bot"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 )
 
-func main() {
- err := config.ReadConfig()
-
- if err != nil {
-  fmt.Println(err)
-  return
- }
-
- bot.Start()
-
- <-make(chan struct{})
- 
-return
+type Config struct {
+    Token string `json:"token"` 
 }
+
+func loadConfig() (Config, error) {
+    // Read the config file
+    configFile, err := os.Open("config.json")
+    if err != nil {
+        return Config{}, fmt.Errorf("error opening config.json: %v", err)
+    }
+    defer configFile.Close()
+
+    jsonBytes, err := ioutil.ReadAll(configFile)
+    if err != nil {
+        return Config{}, fmt.Errorf("error reading config.json: %v", err)
+    }
+
+    // Parse JSON into the Config struct
+    var config Config
+    err = json.Unmarshal(jsonBytes, &config)
+    if err != nil {
+        return Config{}, fmt.Errorf("error parsing config.json: %v", err)
+    }
+
+    return config, nil
+}
+
+func main () {
+	config, err := loadConfig()
+    if err != nil {
+        fmt.Println("Failed to load config:", err)
+        os.Exit(1) // Exit with an error
+    }
+
+	sess, err := discordgo.New("Bot " + config.Token)
+	if err !=  nil {	
+		log.Fatal(err)
+	}
+
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "hello" {
+			s.ChannelMessageSend(m.ChannelID, "World!")
+		}
+	})
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close()
+
+	fmt.Println("Online~")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
+
